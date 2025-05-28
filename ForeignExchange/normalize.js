6c9ae4a14704f4fd7d975d9ad6d877b91096c671
Erhan Sönmez <48383314+erhansonmez@users.noreply.github.com>
2025-05-28 08:41:45 +0300

    Update normalize.js

--- ForeignExchange/normalize.js
+++ ForeignExchange/normalize.js
@@ -1,26 +1,9 @@
-const rateFixRatio = 1.026;
-
 function normalizeRate({ code, name, buyRate, sellRate }) {
-  const val = {
+  return {
     code: code ? code.trim() : null,
     name: name ? name.trim() : null,
     buyRate: (typeof buyRate === "string" ? parseFloat(buyRate.replace(',', '.')) : buyRate) || 0,
     sellRate: (typeof sellRate === "string" ? parseFloat(sellRate.replace(',', '.')) : sellRate) || 0
   };
-
-  /*
-  console.log(
-    "code:"+code, 
-    "sell_rate:"+val.sellRate, 
-    "new_sell_rate:"+val.sellRate * rateFixRatio,
-    "diff:"+((val.sellRate * rateFixRatio) - val.sellRate),
-    "diff_rate:"+((((val.sellRate * rateFixRatio) - val.sellRate) / val.sellRate) * 100).toFixed(4)
-  );
-  */
-
-  val.buyRate = val.buyRate * rateFixRatio;
-  val.sellRate = val.sellRate * rateFixRatio;
-
-  return val;
 }
 module.exports = { normalizeRate };
